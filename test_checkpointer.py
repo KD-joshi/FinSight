d769416f0b405218300adaceec7c8848a11a546e
KD-joshi <kcjoshivrl@gmail.com>
2026-09-10 19:15:22 +0530

    chore: remove irrelevant files from repo and add to .gitignore

--- test_checkpointer.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-from langgraph.checkpoint.sqlite import SqliteSaver
-
-with SqliteSaver.from_conn_string("checkpoints.sqlite") as checkpointer:
-    print(list(checkpointer.list({"configurable": {}})))
